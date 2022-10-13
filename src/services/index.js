a678b364b7fcbc3b3c8492e812ac8b9a41e5d4c9
falc0shka <falc0shka@gmail.com>
2022-10-13 18:43:24 +0300

    Update API

--- src/services/index.js
+++ src/services/index.js
@@ -4,4 +4,3 @@ module.exports.tokenService = require('./token.service');
 module.exports.userService = require('./user.service');
 module.exports.hostsService = require('./hosts.service');
 module.exports.itemsService = require('./items.service');
-       
